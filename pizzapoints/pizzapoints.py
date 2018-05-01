675a1c4a8187557a5c7010b6d44b1f697867a927
Daniel Torres <dani10sa2@hotmail.com>
2018-05-01 23:52:01 +0200

    some changes

--- pizzapoints/pizzapoints.py
+++ pizzapoints/pizzapoints.py
@@ -68,10 +68,10 @@ def initdb_command():
 
 @app.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+    #db = get_db()
+    #cur = db.execute('select title, text from entries order by id desc')
+    #entries = cur.fetchall()
+    return render_template('show_entries.html', entries=[])
 
 
 
